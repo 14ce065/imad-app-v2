b95094d597936d053e7b07ca83b87820d66308c6
14ce065 <14ce065@charusat.edu.in>
2017-02-07 20:49:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/article-three', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/lavi.jpg', function (req, res) {
+app.get('/lavi.jpg', function (req, res) {
    // res.send('Article one wil be server here!');
   res.sendFile(path.join(__dirname, 'ui', 'lavi.jpg'));
 });
