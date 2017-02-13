3ac9896cddf4f0a1e074d23c119ab6deb4a15b57
14ce065 <14ce065@charusat.edu.in>
2017-02-13 18:24:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,4 +20,20 @@ button.onclick=function(){
    request.open('GET','http://user-14ce065.imad.hasura-app.io/counter',true);
    request.send(null);
     
+
+};
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit');
+submit.onclick=function()
+{
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i< names.length;i++)
+    {
+        list+='<li>'+names()+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
 };
