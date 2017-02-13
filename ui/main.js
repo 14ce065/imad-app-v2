00949367b93af79275bdb1b425abfe4caf4b3e84
14ce065 <14ce065@charusat.edu.in>
2017-02-13 18:51:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,12 +28,30 @@ var nameip=nameInput.value;
 var submit=document.getElementById('submit');
 submit.onclick=function()
 {
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i< names.length;i++)
+    
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function()
     {
-        list+='<li>'+names()+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            
+            if(request.status === 200)
+            {
+                var names=['name1','name2','name3'];
+                 var list='';
+                for(var i=0;i< names.length;i++)
+                {
+                    list+='<li>'+names()+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET','http://user-14ce065.imad.hasura-app.io/submit?nameip='+nameip,true);
+   request.send(null);
+    
+   
 };
